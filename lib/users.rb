7af6016e83f0cb163a88b1e07cc573d2cc6e8c42
Shelby Kelly <shelby.kelly@colorado.edu>
2014-07-30 15:37:27 -0600

    first validation

--- lib/users.rb
+++ lib/users.rb
@@ -2,4 +2,7 @@ require "active_record"
 
 class User < ActiveRecord::Base
   has_one :fish
+
+  validates :username, :presence => true, :length => {:minimum => 4}, :uniqueness => true
+  validates :password, :presence => true
 end
